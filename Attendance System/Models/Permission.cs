af95d96c370c4b71e5eeaa68e7189dd8657a1335
ahmedmagdy <ahmedmagdy492@yahoo.com>
2020-03-26 19:44:33 +0200

    added ahmed magdy part

--- Attendance System/Models/Permission.cs
+++ Attendance System/Models/Permission.cs
@@ -13,11 +13,12 @@ namespace Attendance_System.Models
         public int Id { get; set; }
         [Required]
         public string CauseOfAbsence { get; set; }
-        public DateTime PermissionDate { get; set; }
+        [Column(TypeName = "datetime2")]
+        public DateTime? PermissionDate { get; set; }
         public bool IsApproved { get; set; }
         public string Admin { get; set; }
-        public DateTime SendingDate { get; set; }
-        public DateTime ApprovementDate { get; set; }
+        public DateTime? SendingDate { get; set; }
+        public DateTime? ApprovementDate { get; set; }
         [ForeignKey("ApplicationUser")]
         public string UserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
